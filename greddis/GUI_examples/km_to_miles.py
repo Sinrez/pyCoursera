a2f5718a590b01e3aa587a0d0e27d91c300ed1e3
sintez <madsint@gmail.com>
2021-02-03 20:20:30 +0300

    исправление

--- greddis/GUI_examples/km_to_miles.py
+++ greddis/GUI_examples/km_to_miles.py
@@ -5,7 +5,7 @@ import tkinter.messagebox
 class KiloComvertGUI:
     def __init__(self):
 
-        self.main_wind = tkinter.Tk
+        self.main_wind = tkinter.Tk()
 
         #рамки для интерфейса
         self.top_frame = tkinter.Frame(self.main_wind)
@@ -20,27 +20,27 @@ class KiloComvertGUI:
         self.kilo_entry.pack(side = 'left')
 
         #Создаем элементы интерфейса Баттона для нижней рамки
-        self.cals_button = tkinter.Button(self.button_frme, text = 'Конвертировать', command = self.convert())
-        self.quit_button = tkinter.Button(self.button_frme, text = 'Выйти', command = self.main_wind.destroy())
+        self.cals_button = tkinter.Button(self.button_frme, text = 'Конвертировать', command = self.convert)
+        self.quit_button = tkinter.Button(self.button_frme, text = 'Выйти', command = self.main_wind.destroy)
 
         #пакуем кнопки
         self.cals_button.pack(side ='left')
         self.quit_button.pack(side='left')
 
         #пакуем рамки
-        self.top_frame.pack
-        self.button_frme.pack
+        self.top_frame.pack()
+        self.button_frme.pack()
 
         #войти в главный цикл tkinter
         tkinter.mainloop()
 
         #метод конвертации
-        def convert(self):
-            kilo = float(self.kilo_entry.get())
-            miles = kilo * 0.6214
+    def convert(self):
+        kilo = float(self.kilo_entry.get())
+        miles = round(kilo * 0.6214,2)
 
-            #показать результаты в диалоговом окне
-            tkinter.messagebox.showinfo('Результаты', str(kilo) + ' км эквивалентно '+ str(miles) +' милям')
+        #показать результаты в диалоговом окне
+        tkinter.messagebox.showinfo('Результаты', str(kilo) + ' км эквивалентно '+ str(miles) +' милям')
 
 #создать экземпляр класса
-kilo_conv = KiloComvertGUI
+kilo_conv = KiloComvertGUI()

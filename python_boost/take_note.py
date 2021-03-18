355ce87b300dd1de296c95ccc8d42716e9137e71
sintez <madsint@gmail.com>
2021-03-19 01:56:49 +0300

    дополнены функции

--- python_boost/take_note.py
+++ python_boost/take_note.py
@@ -1,9 +1,6 @@
-def take_note(name, str_txt, *tags):
+def take_note(name, str_txt, tags):
     dictionary = dict(name = name, str = str_txt, tags = list(tags))
     return dictionary
-
-# test
-# print(take_note('test_note_1', 'Тестовя заметка 1','заметка','первая','цели'))
 
 all_notes_test = []
 def input_notes():
@@ -15,16 +12,23 @@ def input_notes():
         go_input = input('Ввести еще одну заметку? Y/N: ').upper()
         all_notes_test.append(take_note(note_name, note_text, note_tags))
 input_notes()
-print(all_notes_test)
-# all_notes_test =  [{'name': 'заметка1', 'str': 'раз два три', 'tags': ['зам1', '1зам', '11']},
-# {'name': 'заметкО2', 'str': 'тестовая заметка снова', 'tags': ['зам2', '2зам', '2']}]
 
 def all_notes(tag, notes):
+    res = []
     for dict in notes:
         if tag in dict['tags']:
-            return dict
+            res.append(dict)
+    return ' '.join(map(str, res)).strip('{').strip('}')
 
+def main():
+    the_tag = input('Ввведите тэг для поиска заметки или Q для выхода: ')
+    if the_tag.upper() == 'Q':
+        exit()
+    elif all_notes(the_tag,all_notes_test) is None:
+        print("Нет заметки по такому тэгу")
+    else:
+        print(all_notes(the_tag,all_notes_test))
 
-print(all_notes('зам1',all_notes_test))
+main()
 
 

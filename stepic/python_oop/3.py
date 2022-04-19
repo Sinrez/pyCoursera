ccbe1ba4181b91f3a0eebd4c33171a43d1cff25f
sintez <madsint@gmail.com>
2022-04-19 22:05:04 +0300

    new

---
+++ stepic/python_oop/3.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def full_name(self):
+        return f'{self.last_name} {self.first_name}'
+
+    def is_adult(self):
+        if self.age >= 18:
+            return True
+        else:
+            return False
+
+p1 = Person('Jimi', 'Hendrix', 55)
+print(p1.full_name())  # выводит "Hendrix Jimi"
+print(p1.is_adult()) # выводит "True"

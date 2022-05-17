1a3c8cb07519c4076e1b219ceb8db9fa4fa37125
sintez <madsint@gmail.com>
2022-05-18 00:28:47 +0300

    new пример работы с БД SQLlite

--- python_zero_xakep/hw4/4.1.py
+++ python_zero_xakep/hw4/4.1.py
@@ -64,17 +64,61 @@ def return_all_notes():
     except Exception:
         exit('Что-то сломалось')
 
+def find_notes(search):
+    "поиск заметок в БД"
+    print('-------------')
+    try:
+        # Делаем выборку записей содержащих строку search и в цикле печатаем их значения
+        cursor.execute("""SELECT * FROM pynotes WHERE note_name LIKE ('%' || ? || '%' )""", (search,))
+        row = cursor.fetchone()
+        while row is not None:
+           print(row[0])
+           print(row[1])
+           print(row[2])
+           print('-------------')
+           row = cursor.fetchone()
+
+    except sqlite3.ProgrammingError:
+           exit('Синтаксическая ошибка или нет базы')
+    except sqlite3.OperationalError:
+        exit('Системная ошибка, транзакция не может быть обработана')
+    except Exception:
+        exit('Что-то сломалось')
+
+def check_q(d):
+    if d.lower() == 'q':
+        exit(f'Работа завершена')
+    elif not d.isdigit():
+        exit(f'Нужно ввести число!')
+
 def main():
     "основной блок программы"
-    the_tag = input('Вы работаете с топорной программой ввода заметок, если решили выйти - введите Q, если нет - любую клавишу :) ')
-    if the_tag.upper() == 'Q':
-      exit('Работа завершена, всего доброго!')
-    else:
-        input_notes()
-        print('')
-        return_all_notes()
-        cursor.close()
-        conn.close()
+    the_tag = input('Вы работаете с топорной программой ввода заметок, если решили выйти - введите Q, если нет - любую клавишу :) ').strip().lower()
+    while(the_tag != 'q'):
+        inp_check = (input('''Введите -
+        1 чтобы добавить запись
+        2 чтобы посмотреть записи
+        3 чтобы произвести поиск по записям
+        q чтобы выйти из программы: ''')).strip()
+
+        check_q(inp_check)
+        inp_check = int(inp_check)
+
+        if inp_check == 1:
+            input_notes()
+        elif inp_check == 2:
+            return_all_notes()
+        elif inp_check == 3:
+            print('*' * 20)
+            search = input('''Введите заголовок или часть заголовка статьи которую ищем
+    (регистр букв имеет значение): ''')
+            find_notes(search)
+            print('*' * 20)
+        elif  inp_check == 'q':
+            break
+    # Закрываем соединение с базой данных
+    cursor.close()
+    conn.close()
 
 if __name__ == '__main__':
   main()

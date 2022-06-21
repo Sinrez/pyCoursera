30ce5b3ae756155fd6de985246e38a0661f1e2a2
sintez <madsint@gmail.com>
2022-06-21 21:32:06 +0300

    upd

---
+++ light_python_lybanovich/net/jrpc_client.py
@@ -0,0 +1,4 @@
+from jsonrpcclient import request
+num = 7
+response = request("http://localhost:5000", "double", num=num)
+print("Double", num, "is", response.data.result)
---
+++ light_python_lybanovich/net/jrpc_serv.py
@@ -0,0 +1,9 @@
+from jsonrpcserver import method, serve
+
+@method
+def double(num):
+    return num * 2
+
+if __name__ == '__main__':
+    serve()
+

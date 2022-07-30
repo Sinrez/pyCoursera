daaba8757fe57593ad17e58e6b4702d2c66bd57f
sintez <madsint@gmail.com>
2022-07-31 00:33:48 +0300

    работа с flask

--- learn py/Flask/vsearch4web.py
+++ learn py/Flask/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,escape
 from vl2 import search4letters
 
 app = Flask(__name__)
@@ -19,7 +19,18 @@ def entry_page() -> 'html':
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearh.log','a') as log:
-        print(req, res, file =log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearh.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='View Log',the_row_titles=titles, the_data=contents,)
 
 if __name__ =='__main__':
     app.run(debug=True)

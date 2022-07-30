8cff764bb3952217196b568d63d57d4afd638380
sintez <madsint@gmail.com>
2022-07-30 15:26:49 +0300

    работа с flask

--- learn py/Flask/vsearch4web.py
+++ learn py/Flask/vsearch4web.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from vl2 import search4letters
+
 
 app = Flask(__name__)
 
@@ -10,5 +11,9 @@ def hello() -> str:
 @app.route('/search4')
 def do_search() ->str:
     return str(search4letters('ife, the universe, and everything!', 'eiru,!'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',the_title='Welcome to search4letters on the web!')
 
 app.run()

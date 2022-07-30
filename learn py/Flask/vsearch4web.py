7e32547fba0e1dbb8253a44cc3d7a8236993ef9d
sintez <madsint@gmail.com>
2022-07-30 21:53:27 +0300

    работа с flask

--- learn py/Flask/vsearch4web.py
+++ learn py/Flask/vsearch4web.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request
 from vl2 import search4letters
 
-
 app = Flask(__name__)
-
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
 def do_search() ->str:
@@ -14,10 +9,17 @@ def do_search() ->str:
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
     title = 'Here are your results:» (Ваши результаты: )'
+    log_request(request, results)
     return render_template('results.html', the_phrase = phrase, the_letters = letters, the_results = results, the_title = title,)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',the_title='Welcome to search4letters on the web!')
 
-app.run(debug=True)
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearh.log','a') as log:
+        print(req, res, file =log)
+
+if __name__ =='__main__':
+    app.run(debug=True)

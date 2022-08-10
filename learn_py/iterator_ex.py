579873d6df4696d2942f467809cc370deef42ab2
sintez <madsint@gmail.com>
2022-08-10 15:08:08 +0300

    работа с flask, потоки

---
+++ learn_py/iterator_ex.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+# os.chdir('learn_py/buzzers.csv')
+
+# with open('buzzers.csv') as rd:
+#     print(rd.read())
+
+# with open('buzzers.csv') as rd:
+#     for line in csv.reader(rd):
+#         print(line)
+
+
+
+
+

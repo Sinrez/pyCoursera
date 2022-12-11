1f60197999130c480af1263067414fd71ecb07ee
sintez <madsint@gmail.com>
2022-12-11 21:59:16 +0300

    парсинг курса USD

--- heartbeat_market/3.py
+++ heartbeat_market/3.py
@@ -1,15 +1,23 @@
 from bs4 import BeautifulSoup
 import requests as req
 
-resp = req.get('https://www.banki.ru/products/currency/bank/sberbank/usd/moskva/#bank-rates')
-soup = BeautifulSoup(resp.text, 'html.parser')
-items = soup.find_all("td", class_='font-size-large')
-buy_in = str(list(items)[0])
-#вытаскиваем из тегов покупку usd и продажу
-sale_in  = str(list(items)[1])
+def get_usd_course():
 
-buy_val = float(buy_in[buy_in.find('">')+2:buy_in.find('</')].strip().replace(',','.'))
-# вырезаем из тега число - сумму покупки/продажи
-sale_val = float(sale_in[sale_in.find('">')+2:sale_in.find('</')].strip().replace(',','.'))
+    url_in = 'https://www.banki.ru/products/currency/bank/sberbank/usd/moskva/#bank-rates'
+    resp = req.get(url_in)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    items = soup.find_all("td", class_='font-size-large')
+    buy_in = str(list(items)[0])
+    #вытаскиваем из тегов покупку usd и продажу
+    sale_in  = str(list(items)[1])
 
-print(f'USD Покупка: {buy_val} Продажа {sale_val}')
+    buy_val = float(buy_in[buy_in.find('">')+2:buy_in.find('</')].strip().replace(',','.'))
+    # вырезаем из тега число - сумму покупки/продажи
+    sale_val = float(sale_in[sale_in.find('">')+2:sale_in.find('</')].strip().replace(',','.'))
+
+    return f'USD Покупка: {buy_val} Продажа {sale_val} Спред: {sale_val-buy_val}'
+
+
+
+if __name__ == '__main__':
+    print(get_usd_course())

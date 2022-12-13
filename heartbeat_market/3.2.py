873e553c2b562ab4f05e4888aceddf6d1422d9dc
sintez <madsint@gmail.com>
2022-12-14 00:54:31 +0300

    парсинг курса USD

--- heartbeat_market/3.2.py
+++ heartbeat_market/3.2.py
@@ -5,8 +5,10 @@ import datetime as dt
 from matplotlib import pyplot as plt, MatplotlibDeprecationWarning
 import warnings
 
-conn = sqlite3.connect('usd_spread.sqlite')
-cursor = conn.cursor()
+
+# начальный вариант соединения с БД. Внесен в каждую функцию, чтобы не держать соединение
+# conn = sqlite3.connect('usd_spread.sqlite')
+# cursor = conn.cursor()
 
 def get_usd_course() -> tuple:
     now = dt.date.today()
@@ -27,44 +29,68 @@ def get_usd_course() -> tuple:
 
 def db_create() -> None:
     try:
+        conn = sqlite3.connect('usd_spread.sqlite')
+        cursor = conn.cursor()
         # Создаем таблицу
         cursor.execute(
             '''CREATE TABLE IF NOT EXISTS spread (buy_val float , sale_val float, spread float, cur_date date primary key,unique(cur_date))''')
+        cursor.close()
+        conn.close()
     except sqlite3.ProgrammingError as er0:
         print(f'Таблица не найдена или уже существует: {er0}')
     except sqlite3.Warning as er1:
         exit(f'Ошибка БД {er1}')
 
 
-def add_data() ->None:
+def add_data() -> None:
     mydata = get_usd_course()
     try:
+        conn = sqlite3.connect('usd_spread.sqlite')
+        cursor = conn.cursor()
         cursor.execute("INSERT INTO spread (buy_val, sale_val, spread, cur_date) VALUES (?, ?, ?, ?)", mydata)
         conn.commit()
+        cursor.close()
+        conn.close()
     except sqlite3.IntegrityError as er3:
         print(f'За сегодняшнюю дату {dt.date.today()} уже есть запись в базе! {er3} ')
 
+
 def del_data(del_data) -> None:
+    conn = sqlite3.connect('usd_spread.sqlite')
+    cursor = conn.cursor()
     sql_del_query = """DELETE FROM spread where cur_date = ? """
-    cursor.execute(sql_del_query, (del_data, ))
+    cursor.execute(sql_del_query, (del_data,))
     conn.commit()
+    cursor.close()
+    conn.close()
     print("Запись успешно удалена")
 
+
 def print_all_entries() -> None:
+    conn = sqlite3.connect('usd_spread.sqlite')
+    cursor = conn.cursor()
     cursor.execute('SELECT  * FROM spread')
     row = cursor.fetchone()
     while row is not None:
         print(row)
         row = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
 
 def return_all_entries() -> list:
+    conn = sqlite3.connect('usd_spread.sqlite')
+    cursor = conn.cursor()
     cursor.execute('SELECT  * FROM spread')
     row = cursor.fetchone()
     res = []
     while row is not None:
         res.append(row)
         row = cursor.fetchone()
+    cursor.close()
+    conn.close()
     return res
+
 
 def make_graph(lst) -> None:
     try:
@@ -79,12 +105,13 @@ def make_graph(lst) -> None:
     except Exception as ex0:
         print(f'Ошибка при построении графика {ex0}')
 
+
 if __name__ == '__main__':
     db_create()
     make_graph((return_all_entries()))
     # add_data()
     # print_all_entries()
     # del_data('2022-12-13')
-    #return_all_entries()
-    cursor.close()
-    conn.close()
+    # return_all_entries()
+    # cursor.close()
+    # conn.close()

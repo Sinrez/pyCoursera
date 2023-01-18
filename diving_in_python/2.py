926da9fb06861d3bf8d9f5ab93ffadc84d1143ab
sintez <madsint@gmail.com>
2023-01-18 10:23:13 +0300

    Revert "Задачи из курса Погружение в Python Stepic"
    This reverts commit 7717b013c6e8c92f599eb2f9aad7a51131bf67e6.

--- diving_in_python/2.py
+++
@@ -1,4 +0,0 @@
-
-inp = list(set('Beautiful is better than ugly.'.lower()))
-inp_2 = sorted([i for i in inp if i.isalpha()])
-print(''.join(inp_2))
--- ozon_contest_route256/1.py
+++
@@ -1,4 +0,0 @@
-test_case_count = int(input())
-for _ in range(test_case_count):
-    a, b = map(int, input().split())
-    print(a + b)

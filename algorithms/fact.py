3da2f06e12e218f682e9b7125e88aaa04c6c0b64
sintez <madsint@gmail.com>
2023-02-01 21:45:58 +0300

    algoritms

--- algorithms/fact.py
+++ algorithms/fact.py
@@ -4,4 +4,14 @@ def fact(n):
     else:
         return n*fact(n-1)
 
+def fact_non_rec(n):
+    res = 1
+    if n == 1:
+        return res
+    else:
+        for i in range(2,n+1):
+            res *= i
+    return res
+
 print(fact(5))
+print(fact_non_rec(5))

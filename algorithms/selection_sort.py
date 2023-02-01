e5832a11c263622bb8e9825435e21d0d3688498d
sintez <madsint@gmail.com>
2023-02-01 20:04:58 +0300

    selection sort

---
+++ algorithms/selection_sort.py
@@ -0,0 +1,20 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+if __name__ == '__main__':
+    print(selection_sort([5, 3, 6, 2, 10]))

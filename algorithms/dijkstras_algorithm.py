92ea3846ba65ea0da8ddbe3b7d4344e19cdfb262
sintez <madsint@gmail.com>
2023-02-05 01:19:43 +0300

    algoritms

--- algorithms/dijkstras_algorithm.py
+++ algorithms/dijkstras_algorithm.py
@@ -6,4 +6,25 @@ graph["start"] = {}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
 
-print(*graph['start'].keys())
+# print(*graph['start'].keys())
+
+# включим в граф остальные узлы и из соседей
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+# print(graph)
+
+# the costs table - таблица весов ребер графа
+infinity = float("inf") #бесконечность в - infinity in python
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+

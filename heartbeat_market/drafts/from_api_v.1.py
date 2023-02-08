02edb56eb6497e35eb0d377bdd6810dbf0320725
sintez <madsint@gmail.com>
2023-02-08 18:35:33 +0300

    upd code

--- heartbeat_market/drafts/from_api_v.1.py
+++ heartbeat_market/drafts/from_api_v.1.py
@@ -2,7 +2,7 @@ import requests as re
 from heartbeat_market.user_agent import make_usr_agent
 
 url_in = 'https://alfabank.ru/api/v1/scrooge/currencies/alfa-rates?currencyCode.in=USD,EUR,CNY,GBP,CHF,CZK,TRY' \
-      '&rateType.in=rateCass,makeCash&lastActualForDate.eq=true&clientType.eq=standardCC&date.lte=2023-01-29T20:28:03%2B03:00'
+      '&rateType.in=rateCass,makeCash&lastActualForDate.eq=true&clientType.eq=standardCC&date.lte=2023-02-08T20:28:03%2B03:00'
 
 resp = re.get(url=url_in,headers=make_usr_agent())
 print(resp.json())
